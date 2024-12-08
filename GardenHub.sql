ea8df72ee7df1abca322bd63e98f6c0d58a3ae50
İclal Ertürk <96770034+iclalerturk@users.noreply.github.com>
2024-12-08 16:03:07 +0300

    Create GardenHub.sql

---
+++ GardenHub.sql
@@ -0,0 +1,65 @@
+create sequence kullanici_id_seq
+minvalue 1
+increment by 1
+
+CREATE TABLE kullanicilar (
+    isim VARCHAR(20) NOT NULL,
+    soyisim VARCHAR(20) NOT NULL,
+    kullanici_id INT NOT NULL PRIMARY KEY,
+    mail VARCHAR(70) NOT NULL,
+    sifre VARCHAR(20) NOT NULL,
+    bdate DATE,
+    butce INT DEFAULT 0,
+    user_type VARCHAR(50) CHECK (user_type IN ('Kiraci', 'Kullanici')),
+    CONSTRAINT yas_sinir CHECK (AGE(CURRENT_DATE, bdate) >= INTERVAL '18 years')
+);
+
+create table yonetici(
+	mail varchar(70),
+	sifre varchar(20)
+);
+
+create table urunler(
+	urun_adi varchar(20) not null,
+	kg int,
+	fiyat int NOT NULL CHECK (Fiyat > 0),
+	sahip_id int references kullanicilar(kullanici_id)	
+);
+
+
+CREATE TABLE Ekipman (
+	ekipman_id int not null primary key,
+	ekipman_adi varchar(20) not null,
+	ekipman_sayisi int not null,
+    fiyat NUMERIC(10,2) NOT NULL CHECK (fiyat > 0)
+);
+
+-- 2. Bahçeler Tablosu
+CREATE TABLE Bahceler (
+	bahce_id int NOT NULL PRIMARY KEY,
+	alan NUMERIC(5,2) NOT NULL,
+	konum varchar(20),
+	toprak_tipi varchar(20),
+    durum VARCHAR(20) NOT NULL CHECK (Durum IN ('Bos', 'Kiralanmis')),
+    fiyat NUMERIC(10,2) NOT NULL CHECK (Fiyat > 0)
+);
+
+-- 3. Kiralamalar Tablosu(eker)
+CREATE TABLE Kiralamalar (
+    kiralama_id int PRIMARY KEY,
+    kullanici_id INT REFERENCES kullanicilar(kullanici_id),
+    bahce_id INT REFERENCES Bahceler(bahce_id),
+    baslangic_tarihi DATE NOT NULL,
+    sure INT NOT NULL CHECK (sure > 0),
+    UNIQUE (kullanici_id, bahce_id)
+);
+
+CREATE TABLE EkipmanTalep (
+    talep_id int PRIMARY KEY,
+    kullanici_id INT REFERENCES Kullanicilar(kullanici_id),
+    ekipman_id INT REFERENCES Ekipman(ekipman_id),
+    miktar INT CHECK (Miktar > 0),
+    talep_tarihi DATE DEFAULT CURRENT_DATE
+);
+
+
