b9ded8da8647c6bc97847bdeb4be5b4c73a4828f
İclal Ertürk <96770034+iclalerturk@users.noreply.github.com>
2024-12-24 18:09:09 +0300

    Create connectTopstgre.py

---
+++ connectTopstgre.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+hostname = 'localhost'
+username = 'postgres'
+database = 'GardenHub'
+password = '1234'
+port_id = '5432'
+try:
+    conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database, port=port_id)
+
+    """ query = "insert into yonetici(mail,sifre) values('admin@hotmail.com','admin')"
+    preparedStatement = conn.prepareStatement(query)
+    preparedStatement.executeQuery()
+
+    query = "select * from yonetici"
+    preparedStatement = conn.prepareStatement(query)
+    resultSet = preparedStatement.executeQuery(query)
+
+    while resultSet.next():
+        print(resultSet.getString(1), resultSet.getString(2)) """
+    
+    cur = conn.cursor()
+    query = ''' insert into yonetici(mail,sifre) values('admin@hotmail.com','admin') '''
+    cur.execute(query)
+    conn.commit()
+    query = ''' select * from yonetici '''
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+except Exception as e:
+    print("Error: ", e)
