60251ec6a7d64774fbd3e84ea2f60a2b328ccb74
seraycelik <162334443+seraycelik@users.noreply.github.com>
2024-12-29 19:09:30 +0300

    Bahce sınıfı

---
+++ arayuzler/bahce_class.py
@@ -0,0 +1,45 @@
+import psycopg2
+
+class Bahce:
+    def __init__(self, bahce_id, alan, konum, toprak_tipi, durum, fiyat):
+        self.bahce_id = bahce_id
+        self.alan = alan
+        self.konum = konum
+        self.toprak_tipi = toprak_tipi
+        self.durum = durum
+        self.fiyat = fiyat
+
+
+    @staticmethod
+    def get_bahce_from_db(bahce_id):
+        hostname = 'localhost'
+        username = 'postgres'
+        database = 'GardenHub'
+        password = '1234'
+        port_id = '5432'
+        try:
+            conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database, port=port_id
+                    
+            )
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT * FROM Bahceler WHERE bahce_id = %d", (bahce_id))
+            user_data = cursor.fetchone()
+            conn.close()
+            if bahce_id:
+                return Bahce(
+                    bahce_id=user_data[0],
+                    alan=user_data[1],
+                    konum=user_data[2],
+                    toprak_tipi=user_data[3],
+                    durum=user_data[4],
+                    fiyat=user_data[5],
+                    
+                )
+            return None
+        except Exception as e:
+            print("Error: ", e)
+        
+
+
+
